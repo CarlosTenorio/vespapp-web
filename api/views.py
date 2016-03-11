ba4ff349a72180b00f8956b6d6a0a63020ae16f6
Joan Font <joanfont@gmail.com>
2016-03-11 15:14:11 +0100

    debug

--- api/views.py
+++ api/views.py
@@ -26,6 +26,8 @@ class SightingPictureCreateView(ListCreateAPIView):
         return Picture.objects.filter(sighting__pk=sighting_id)
     
     def create(self, request, *args, **kwargs):
+        import ipdb
+        ipdb.set_trace()
         sighting_id = self.kwargs.get('sighting_id')
         request.data['sighting'] = Sighting.objects.get(pk=sighting_id)
         return super(SightingPictureCreateView, self).create(request, *args, **kwargs)
