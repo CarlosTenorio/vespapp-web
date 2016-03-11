e4c6eac369e21b997e6b62fa21bc1b8062d6e687
Carlos Tenorio <carlostenorioperez@hotmail.com>
2016-03-11 13:25:40 +0100

    create sighting_expert_comments view and add text to faq view - Carlos

--- web/views.py
+++ web/views.py
@@ -8,7 +8,8 @@ class HomePageView(TemplateView):
 class FAQView(TemplateView):
     template_name = "faq.html"
 
-#class SightingExpertCommentsView(TemplateView):
-#    template_name = "sighting_expert_comments.html"
+class SightingExpertCommentsView(TemplateView):
+    template_name = "sighting_expert_comments.html"
+
 #    def get_context_data(self, **kwargs):
-#        sighting_id = sighting.objects.get(id=kwargs['sighting_id'])
+#        sighting_id = kwargs['sighting_id']
