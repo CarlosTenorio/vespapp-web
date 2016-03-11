1498710019bc0f91f1220a1afafed6e53161aea2
Joan Font <joanfont@gmail.com>
2016-03-11 13:34:51 +0100

    try

--- api/serializers.py
+++ api/serializers.py
@@ -9,5 +9,11 @@ class SightingSerializer(serializers.ModelSerializer):
 
 
 class PictureSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        validated_data['sighting'] = 1
+        return super().create(validated_data)
+
     class Meta:
         model = Picture
+        fields = ('file',)
