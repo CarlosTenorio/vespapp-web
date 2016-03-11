5689106b386cbc8bda21bfc3df6d639223c83bd0
Joan Font <joanfont@gmail.com>
2016-03-11 10:25:13 +0100

    urls commented

--- api/urls.py
+++ api/urls.py
@@ -6,20 +6,20 @@ urlpatterns = [
 
     url(r'^sightings/(?P<sighting_id>[0-9]+)/$',
         api_views.SightingRetrieveUpdateView.as_view(), name='sightings_retrieve_update'),
-
-    url(r'^sightings/(?P<sighting_id>[0-9]+)/photos/$',
-        views.sighting_photos),
-
-    url(r'^sightings/(?P<sighting_id>[0-9]+)/expert_comments/$',
-        views.sighting_expert_comments),
-    url(r'^sightings/(?P<sighting_id>[0-9]+)/expert_comments/(?P<expert_comment_id>[0-9]+)$',
-        views.sighting_expert_comment),
-
-    url(r'^sightings/(?P<sighting_id>[0-9]+)/questions/$',
-        views.sighting_questions),
-    url(r'^sightings/(?P<sighting_id>[0-9]+)/questions/(?P<question_id>[0-9]+)$',
-        views.sighting_question),
-
-    url(r'^locations/$',
-        views.locations),
+    #
+    # url(r'^sightings/(?P<sighting_id>[0-9]+)/photos/$',
+    #     views.sighting_photos),
+    #
+    # url(r'^sightings/(?P<sighting_id>[0-9]+)/expert_comments/$',
+    #     views.sighting_expert_comments),
+    # url(r'^sightings/(?P<sighting_id>[0-9]+)/expert_comments/(?P<expert_comment_id>[0-9]+)$',
+    #     views.sighting_expert_comment),
+    #
+    # url(r'^sightings/(?P<sighting_id>[0-9]+)/questions/$',
+    #     views.sighting_questions),
+    # url(r'^sightings/(?P<sighting_id>[0-9]+)/questions/(?P<question_id>[0-9]+)$',
+    #     views.sighting_question),
+    #
+    # url(r'^locations/$',
+    #     views.locations),
 ]
