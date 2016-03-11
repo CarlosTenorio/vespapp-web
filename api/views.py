8a5cadf2011f86da874ebff7b52128639e64f45d
Joan Font <joanfont@gmail.com>
2016-03-11 15:33:19 +0100

    ou yea

--- api/views.py
+++ api/views.py
@@ -28,11 +28,6 @@ class SightingPictureCreateView(ListCreateAPIView):
         return Picture.objects.filter(sighting__pk=sighting_id)
     
     def create(self, request, *args, **kwargs):
-        import ipdb
-        ipdb.set_trace()
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        request.data['sighting'] = self.kwargs.get('sighting_id')
+        return super().create(request, *args, **kwargs)
 
