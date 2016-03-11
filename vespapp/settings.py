2e5052014b94f83002d0c8e01b91701e68064b46
Joan Font <joanfont@gmail.com>
2016-03-11 11:25:56 +0100

    added rest to installed apps

--- vespapp/settings.py
+++ vespapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'web',
     'api',
 ]
@@ -134,3 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
