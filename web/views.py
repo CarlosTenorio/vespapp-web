8c8dd2732ad82f03d5747cf653a629f3b87688fc
Joan Font <joanfont@gmail.com>
2016-03-12 11:46:54 +0100

    resolve conflicts

--- web/views.py
+++ web/views.py
@@ -4,59 +4,50 @@ from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-<<<<<<< HEAD
 from api.models import Sighting
 
 from api.models import SightingFAQ
-=======
 
 from api.models import Sighting
 from api.models import UserComment
->>>>>>> 08f1338... User sighting comments are visualized. URLs changedgit status !
+
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['new_sighting'] = reverse('new_sighting')
         return context
+
 
 class FAQView(ListView):
     template_name = "faq.html"
     model = SightingFAQ
 
 
-<<<<<<< HEAD
 class SightingExpertCommentsView(ListView):
     template_name = "sighting_expert_comments.html"
 
 
-class SightingView(DetailView):
-    template_name = "sighting.html"
-    pk_url_kwarg= "sighting_id"
-    model= Sighting
-
-
 class SightingsView(ListView):
     template_name = "sightings.html"
-    model = Sighting# Defino el modelo que utilizo
-    context_object_name = "sightings_list"# Defino la lista donde se cargan los objetos del modelo
+    model = Sighting  # Defino el modelo que utilizo
+    context_object_name = "sightings_list"  # Defino la lista donde se cargan los objetos del modelo
     paginate_by = 50  # Control de la paginacion
 
     def get_queryset(self, **kwargs):
         return Sighting.objects.all()
-=======
+
+
 class SightingView(DetailView):
-    template_name = "sighting.html"  
+    template_name = "sighting.html"
     pk_url_kwarg = 'sighting_id'
     model = Sighting
 
-class SightingsView(ListView):
-    template_name = "sightings.html"   
-    
     def get_queryset(self, **kwargs):
-     	return Sighting.objects.all()
->>>>>>> 08f1338... User sighting comments are visualized. URLs changedgit status !
+        return Sighting.objects.all()
+
 
 class SightQuestionView(DetailView):
     template_name = "sight_question.html"
@@ -66,27 +57,23 @@ class LocationsPageView(TemplateView):
     template_name = "locations.html"
 
 
-<<<<<<< HEAD
-class SightingCommentsView(ListView):
-    template_name = "sighting_comments.html"
-
-
 class SightExpertCommentView(DetailView):
     template_name = "sight_expert_comment.html"
 
+
 class NewSightingView(TemplateView):
     template_name = "new_sighting.html"
-=======
+
+
 class SightingCommentView(DetailView):
     template_name = "sighting_comment.html"
     template_name_field = 'object'
     model = UserComment
-      
+
     def get_object(self, queryset=None):
         sighting_id = self.kwargs.get('sighting_id')
         comment_id = self.kwargs.get('comment_id')
 
-        sighting = Sighting.objects.get(pk=sighting_id)
         comment = UserComment.objects.get(pk=comment_id)
 
         if str(comment.sighting.id) != str(sighting_id):
@@ -94,19 +81,11 @@ class SightingCommentView(DetailView):
 
         return comment
 
-    
+
 class SightingCommentsView(ListView):
     template_name = "sighting.html"
-    model = UserComment# Defino el modelo que utilizo
-    context_object_name = "user_comment_list"# Defino la lista donde se cargan los objetos del modelo
+    model = UserComment  # Defino el modelo que utilizo
+    context_object_name = "user_comment_list"  # Defino la lista donde se cargan los objetos del modelo
 
     def get_queryset(self, **kwargs):
         return UserComment.objects.all()
-
-	
-class SightExpertCommentView(DetailView):
-	template_name = "sight_expert_comment.html"
-
-class SightingExpertCommentsView(ListView):
-    template_name = "sighting_expert_comments.html"
->>>>>>> 08f1338... User sighting comments are visualized. URLs changedgit status !
