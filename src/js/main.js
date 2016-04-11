a1347455e5e2a39fee73821be6e5e6e8b4065ad2
CamPerez <camperezarevalo@hotmail.com>
2016-04-11 23:32:37 +0200

    sign up form with green style

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,18 @@
       keyboard: true
     });
   });
+
+
+
+ $("#formSignup").addClass("zoomInDown");
+  $("#submit").on('click', function(e){
+    e.preventDefault();
+    $("#formSignup").addClass("zoomOutUp");
+    $('#formSignup').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $("body").css('background', '#66be44');
+      $(".message").css('opacity', 1);
+    });
+  });
+
+
 })(window, jQuery);
