17edc0008f42acd91cc6e8f58d4679fafe2d46f0
CarlosTenorio <carlostenorioperez@hotmail.com>
2016-04-20 20:34:51 +0200

    Security fixed on sightQuestions

--- web/views.py
+++ web/views.py
@@ -8,6 +8,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.core.context_processors import csrf
 from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404  
 
 import json
 
@@ -92,40 +93,60 @@ class SightExpertCommentView(DetailView):
 class SightQuestionView(TemplateView):
 
     @csrf_exempt
-    def sight_question(request, sighting_id, question_order):
+    def sight_question(request):
 
-        s = Sighting.objects.get(id=sighting_id)   
+        if "session_id" in request.session:
+                    
+            sighting_id = request.session['session_id']
+            question_order = request.session['question_order'] + 1 
+            print ("ORDER: " + str(question_order))
 
-        if request.POST:
-            form_question = QuestionForm(request.POST)   
+            request.session['question_order'] = question_order
 
-            if form_question.is_valid():
-                if request.FILES == None:
-                    raise Http404("No objects uploaded")    
+            s = Sighting.objects.get(id=sighting_id)   
 
-                myArray = request.POST.pop('value')
+            if request.POST:
+                form_question = QuestionForm(request.POST)   
 
-                for x in myArray:
-                    s.answers.add(x)
+                if form_question.is_valid():
+                    if request.FILES == None:
+                        raise Http404("No objects uploaded")    
 
-                q = Question.objects.filter(order=int(question_order)+1, sighting_type=s.type)
-                if q.exists():           
-                    url = reverse('sight_question', kwargs={'sighting_id': sighting_id, 'question_order': int(question_order)+1})
-                    return HttpResponseRedirect(url)
+                    myArray = request.POST.pop('value')
+
+                    for x in myArray:
+                        s.answers.add(x)
+
+                    q = Question.objects.filter(order=int(question_order), sighting_type=s.type)
+                    if q.exists():           
+                        #Subtracting one so that the next if it comes from one form to add beginning work properly
+                        question_order = request.session['question_order'] - 1                         
+                        request.session['question_order'] = question_order
+                        url = reverse('sight_question')
+                        return HttpResponseRedirect(url)
+                    else:
+                        mensaje='Regístrate para conocer el estado de tu avispamiento, enviar comentarios y mucho más'    
+                        return render(request, 'home.html', {'mensaje': mensaje})                    
+            else:
+                form_question = QuestionForm()
+
+                q = Question.objects.filter(order=int(question_order), sighting_type=s.type)
+                if q.exists():
+                    context = {
+                        'sighting': Sighting.objects.get(id=sighting_id),
+                        'question': Question.objects.get(order=question_order, sighting_type=s.type),
+                        'answer': Answer.objects.all()
+                    }
+                    return render_to_response('sight_question.html', context=context)
                 else:
                     mensaje='Regístrate para conocer el estado de tu avispamiento, enviar comentarios y mucho más'    
-                    return render(request, 'home.html', {'mensaje': mensaje})                    
+                    return render(request, 'home.html', {'mensaje': mensaje})
+                # url = reverse('home')
+                # return HttpResponseRedirect(url)
         else:
-            form_question = QuestionForm()
-
-        context = {
-            'sighting': Sighting.objects.get(id=sighting_id),
-            'question': Question.objects.get(order=question_order, sighting_type=s.type),
-            'answer': Answer.objects.all()
-        }
-        return render_to_response('sight_question.html', context=context)
-
-
+            # url = reverse('404')
+            # return HttpResponseRedirect(url)
+            raise Http404
 class NewSightingView(TemplateView):
 
     @csrf_exempt
@@ -154,7 +175,10 @@ class NewSightingView(TemplateView):
                     picture_id = Picture()
                     picture_id.sighting = sighting_id
                     picture_id.file.save(f.name, f)
-                    picture_id.save()              
+                    picture_id.save()
+
+                request.session['session_id'] = sighting_id.id   
+                request.session['question_order'] = 0  
 
                 return HttpResponse(sighting_id.pk)
         else:
