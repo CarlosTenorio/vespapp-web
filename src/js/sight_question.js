53fb54998549189daf8c5c6fa13cf5b0b603710b
CarlosTenorio <carlostenorioperez@hotmail.com>
2016-05-24 17:06:18 +0200

    Clean some files

--- src/js/sight_question.js
+++ src/js/sight_question.js
@@ -1,13 +1,3 @@
-// $('.questionRadio').click(function(){
-//    if ($(this).hasClass('active')) {
-//    	//do nothing
-//    }else{
-//    	$('.questionRadio label').addClass('active');
-//    	//$(this).addClass('active');
-//    }
-// });
-
-
 $(document).ready(function(){
     $(".questionRadio").click(function(){
         $(".questionRadio").removeClass("active");
